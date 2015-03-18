a5bb104b000096d4196762e47ed6f362c782d5f0
ana.ribas <ana.ribas@upcnet.es>
2015-03-18 11:43:53 +0100

    link url in slider definition

--- vilaix/core/content/slider.py
+++ vilaix/core/content/slider.py
@@ -3,10 +3,10 @@ from five import grok
 from zope import schema
 
 from plone.directives import form
-
+from plone.indexer.decorator import indexer
 from plone.app.textfield import RichText
 from plone.namedfile.field import NamedImage
-
+from plone.app.contenttypes.utils import replace_link_variables_by_paths
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory("vilaix")
 
@@ -26,6 +26,20 @@ class ISlider(form.Schema):
         description=_(u"Afegeix la descripció que vols que es mostri per temes accessibilitat"),
         required=False,
     )
+
+    remoteUrl = schema.TextLine(
+    title=_(u"url"),
+    description=_(u"URL to open"),
+    required=False,
+
+    )
+
+@indexer(ISlider)
+def getRemoteUrl(obj):
+    if obj.remoteUrl:
+        return replace_link_variables_by_paths(obj, obj.remoteUrl)
+grok.global_adapter(getRemoteUrl, name='getRemoteUrl')
+
 
     # URLdesti = schema.TextLine(
     #     title=_(u"URLdesti"),
